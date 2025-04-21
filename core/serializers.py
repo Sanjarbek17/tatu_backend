8d7b6962117013dd7eb3768ed22ebd0fb07b3b27
Sanjarbek17 <sanjarsaidov173@gmail.com>
2025-04-21 14:11:19 +0500

    Refactor serializers and update URLs to serve static files in development

--- core/serializers.py
+++ core/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 from .models import Article, SchoolYear
 
-class ArticleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Article
-        fields = ['id', 'title', 'description', 'file', 'created_at', 'professor', 'school_year']
-
 class SchoolYearSerializer(serializers.ModelSerializer):
     class Meta:
         model = SchoolYear
         fields = ['id', 'name', 'start_date', 'end_date']
+        
+class ArticleSerializer(serializers.ModelSerializer):
+    school_year = SchoolYearSerializer()
+
+    class Meta:
+        model = Article
+        fields = ['id', 'title', 'description', 'file', 'created_at', 'professor', 'school_year']
--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework.authtoken.views import obtain_auth_token
 from .views import RegisterView, ArticleListView, ArticleDetailView, SchoolYearListView, ProfessorArticlesView, CustomLoginView, ArticleUploadView
 
@@ -12,3 +14,8 @@ urlpatterns = [
     path('api/custom-login/', CustomLoginView.as_view(), name='custom-login'),
     path('api/articles/upload/', ArticleUploadView.as_view(), name='article-upload'),
 ]
+
+# Serve static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
